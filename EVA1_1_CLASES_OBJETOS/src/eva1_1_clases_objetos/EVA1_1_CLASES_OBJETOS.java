092a37cd193d82c0133c0a0ff6065ff9401f325d
MichelleMartinezPosada24 <158379972+MichelleMartinezPosada24@users.noreply.github.com>
2024-02-02 19:26:26 -0600

    Add files via upload

---
+++ EVA1_1_CLASES_OBJETOS/src/eva1_1_clases_objetos/EVA1_1_CLASES_OBJETOS.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_1_clases_objetos;
+
+/**
+ *
+ * @author Familia
+ */
+public class EVA1_1_CLASES_OBJETOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Persona perso1;//declaro un identificar (variable)de tipo Persona
+       //solo he declarado la varible, no he creado el objeto
+       // Crear el obbjetivo (intanciar) consiste en asignar memoria
+       int valor;
+    }
+    
+}
+//CLASE PERSONA
+/*
+//NombreDeLaClase--> Nomeclatura CamelCase
+class NombreDeLaClase {cuerpo de la clase }*/
+class Persona{
+    
+}
+
+
+
+
+
+
+
+
