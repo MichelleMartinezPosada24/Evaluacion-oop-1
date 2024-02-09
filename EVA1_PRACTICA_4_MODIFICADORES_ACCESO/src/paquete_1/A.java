90ac964043eeb2aec7fdf5345ae42f81231ff9a3
MichelleMartinezPosada24 <158379972+MichelleMartinezPosada24@users.noreply.github.com>
2024-02-08 19:13:31 -0600

    Add files via upload

---
+++ EVA1_PRACTICA_4_MODIFICADORES_ACCESO/src/paquete_1/A.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete_1;
+
+import static java.awt.PageAttributes.MediaType.B;
+import static java.awt.PageAttributes.MediaType.C;
+
+/**
+ *
+ * @author Familia
+ */
+public class A {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        one objl = new one();
+        System.out.println(objl);
+        //clase del paquete 2
+        B objB = new B();
+        System.out.println(objB);
+        // two obj2 =new two
+        
+        //three obj3 = new three(;
+        C objC = new c ();
+        System.out.println(objC);
+       
+    }
+    
+}
+class one{
+    
+}
